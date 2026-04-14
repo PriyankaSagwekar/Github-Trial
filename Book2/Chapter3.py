9e8d57f5b6a40731900971e0dfbd37da5e731459
Priyanka Sagwekar <priyankasag10@gmail.com>
2026-04-15 03:15:22 +0530

    chapter 3 of book 2 body added

--- Book2/Chapter3.py
+++ Book2/Chapter3.py
@@ -1,2 +1,4 @@
 # Chapter 3 of book 2
 print("Chapter 3: The New Journey")
+
+print("The new journey takes our hero to uncharted territories, where they encounter new allies and formidable foes. The stakes are higher than ever as they navigate through the unknown.")
